526afbf36636a8c46e5965b0256fe1a54fafb28e
Vitalii Radaiev <hf.chelenger@gamil.com>
2021-03-16 16:50:08 +0200

    fix

--- #src/js/blocks/map.js
+++ #src/js/blocks/map.js
@@ -1,21 +1,63 @@
 {
+	window.onload = () => {
+		let mapNav = document.querySelector('.map-block__nav');
+		if(mapNav) {
+			let items = mapNav.querySelectorAll('.map-block__nav-item');
+			let markers = [
+				[
+					new google.maps.LatLng(49.07715, 33.42683),
+					new google.maps.LatLng(49.4102183495809, 32.053993300771204),
+				],
+				[new google.maps.LatLng(49.59658749073296, 34.53045912467009),],
+				[
+					new google.maps.LatLng(50.454879437956734, 30.510714035328963),
+					new google.maps.LatLng(50.460999524974795, 30.344545827774628),
+					new google.maps.LatLng(50.00141174816132, 36.23466862294647),
+					new google.maps.LatLng(49.950187390392045, 36.16463078339878),
+				],
+			];
+
+			items.forEach((item, index) => {
+				item.addEventListener('click', (e) => {
+					e.preventDefault();
+					item.classList.add('active');
+
+					markersPosition = markers[index];
+					updateMarkers();
+					
+					items.forEach(i => {
+						if(i == item) {
+							return
+						}
+
+						i.classList.remove('active');
+					})
+				})
+			})
+		}
+	}
 
 
 	var isMap = document.getElementById("map");
 	if(isMap) {
 		var map;
-
+		var markersPosition;
 		var center = {
 			lat: 49.07715,
 			lng: 33.42683,
 		}
 
-		var markerPosition = {
-			lat: 49.07715,
-			lng: 33.42683,
-		}
+
+
+		var markers = [];
 
 		function initMap() {
+
+			markersPosition = [
+				new google.maps.LatLng(49.07715, 33.42683),
+				new google.maps.LatLng(49.4102183495809, 32.053993300771204),
+			]
+			// markersPosition = [new google.maps.LatLng(49.59658749073296, 34.53045912467009),]; Полтава
 
 			map = new google.maps.Map(document.getElementById('map'), {
 				center: {lat: center.lat, lng: center.lng},
@@ -80,18 +122,39 @@
 				  ]
 			});
 
-			var marker = new google.maps.Marker({
+			drop()
+		}
 
-			    position: {lat: markerPosition.lat, lng: markerPosition.lng},
+		function drop() {
+			for (var i = 0; i < markersPosition.length; i++) 
+			 {
+			   markers.push(new google.maps.Marker({
+			   position: markersPosition[i],
+			   map: map,
+			   icon: 'img/icons/local.svg',
+			   }));
+			 }
+		}
 
-			    map: map,
+		function setMapOnAll(map) {
+			for (let i = 0; i < markers.length; i++) {
+			  markers[i].setMap(map);
+			}
+		  }
+		  
 
-			    title: '',
-			    label: '',
-
-			    icon: 'img/icons/local.svg',
-			});
-
+		function updateMarkers() {
+			setMapOnAll(null)
+			
+			for (var i = 0; i < markersPosition.length; i++) 
+			{
+			  markers.push(new google.maps.Marker({
+			  position: markersPosition[i],
+			  map: map,
+			  icon: 'img/icons/local.svg',
+			  }));
+			}
+		
 		}
 	}
 }
